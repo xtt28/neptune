9df866e2bb662d0850a4bf704a2dd4a60b24267e
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-06-25 14:09:53 -0400

    Create dedicated function for sending players to spawn

---
+++ game/spawn.go
@@ -0,0 +1,20 @@
+package game
+
+import (
+	"github.com/df-mc/dragonfly/server/player"
+	"github.com/df-mc/dragonfly/server/player/title"
+	"github.com/df-mc/dragonfly/server/world"
+	"github.com/go-gl/mathgl/mgl64"
+	"github.com/sandertv/gophertunnel/minecraft/text"
+	"github.com/xtt28/neptune/config"
+)
+
+func SendToSpawn(p *player.Player, sendTitle bool) {
+	spawn := config.NConfig.Locations.Spawn
+	p.Teleport(mgl64.Vec3{spawn[0], spawn[1], spawn[2]})
+	p.SetGameMode(world.GameModeAdventure)
+	
+	if sendTitle {
+		p.SendTitle(title.New(text.Colourf("<red><bold>Respawned</bold></red>")).WithSubtitle(text.Colourf("<grey>You have respawned.</grey>")))
+	}
+}
