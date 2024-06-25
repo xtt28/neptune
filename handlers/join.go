63b86eda57e289f19502bccb3f0f76d45a4d1bcf
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-06-25 09:57:10 -0400

    Update join permission debug message

--- handlers/join.go
+++ handlers/join.go
@@ -7,7 +7,5 @@ import (
 )
 
 func handleJoin(db *gorm.DB, p *player.Player) {
-	perm := models.Permission{}
-	db.First(&perm, models.Permission{Subject: p.UUID()})
-	p.Messagef("your permission level is %d", perm.Level)
+	p.Messagef("your permission level is %d", models.PermLevel(db, p.UUID()))
 }
