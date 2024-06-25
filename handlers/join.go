19128bbd510c157964a52c3d701247d58d7cf180
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-06-25 13:01:06 -0400

    Create base for scoreboard HUD

--- handlers/join.go
+++ handlers/join.go
@@ -4,9 +4,12 @@ import (
 	"github.com/df-mc/dragonfly/server/player"
 	"github.com/df-mc/dragonfly/server/player/title"
 	"github.com/sandertv/gophertunnel/minecraft/text"
+	"github.com/xtt28/neptune/scoreboard"
 	"gorm.io/gorm"
 )
 
 func handleJoin(db *gorm.DB, p *player.Player) {
+	scoreboard.Render(p)
+
 	p.SendTitle(title.New(text.Colourf("<diamond><bold>Welcome</bold></diamond>")).WithSubtitle(text.Colourf("<grey>to Neptune</grey>")))
 }
---
+++ scoreboard/scoreboard.go
@@ -0,0 +1,23 @@
+package scoreboard
+
+import (
+	"time"
+
+	"github.com/df-mc/dragonfly/server/player"
+	"github.com/df-mc/dragonfly/server/player/scoreboard"
+	"github.com/sandertv/gophertunnel/minecraft/text"
+)
+
+func Render(p *player.Player) {
+	s := scoreboard.New(text.Colourf("<diamond><bold>NEPTUNE</bold></diamond>"))
+	s.Set(0, text.Colourf("<grey>%s</grey>", time.Now().Format("01.02.2006")))
+	s.Set(1, text.Colourf("<white>  </white>"))
+	s.Set(2, text.Colourf("<white><bold> Stats</bold></white>"))
+	s.Set(3, text.Colourf("<dark-grey> » </dark-grey><diamond>Kills</diamond> <white>0</white>"))
+	s.Set(4, text.Colourf("<dark-grey> » </dark-grey><diamond>Deaths</diamond> <white>0</white>"))
+	s.Set(5, text.Colourf("<dark-grey> » </dark-grey><diamond>Bits</diamond> <white>0</white>"))
+	s.Set(6, text.Colourf("<grey></grey>"))
+	s.Set(7, text.Colourf("<diamond>   serverip.here</diamond>"))
+
+	p.SendScoreboard(s)
+}
