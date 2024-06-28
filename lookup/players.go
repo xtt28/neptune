53fc976bee834182a854e0616368b9745fe51d63
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-06-28 09:08:28 -0400

    Add function for case-insensitive online player lookup

--- lookup/players.go
+++ lookup/players.go
@@ -4,13 +4,30 @@ import (
 	"strings"
 
 	"github.com/df-mc/dragonfly/server"
+	"github.com/df-mc/dragonfly/server/player"
 	"github.com/google/uuid"
 	"github.com/xtt28/neptune/database/model"
 	"gorm.io/gorm"
 )
 
+// Taken from sliceutil in package github.com/df-mc/dragonfly/server/internal/sliceutil
+func searchValue[A any, S ~[]A](v S, f func(a A) bool) (a A, ok bool) {
+	for _, val := range v {
+		if f(val) {
+			return val, true
+		}
+	}
+	return
+}
+
+func GetOnlinePlayerCaseInsensitive(srv *server.Server, username string) (*player.Player, bool) {
+	return searchValue(srv.Players(), func(p *player.Player) bool {
+		return strings.EqualFold(p.Name(), username)
+	})
+}
+
 func GetOnlineOrOfflineUUID(db *gorm.DB, srv *server.Server, username string) (id uuid.UUID, online bool, err error) {
-	p, ok := srv.PlayerByName(username)
+	p, ok := GetOnlinePlayerCaseInsensitive(srv, username)
 	if ok {
 		return p.UUID(), true, nil
 	}
