e483838c141ce7c9a6f266f2099dd6407a9cac18
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-06-28 08:09:47 -0400

    Store last username with UUID in user_profiles table upon login

--- database/connector.go
+++ database/connector.go
@@ -10,8 +10,10 @@ var DB *gorm.DB
 
 func ConnectSQLite3(filename string) (conn *gorm.DB, err error) {
 	conn, err = gorm.Open(sqlite.Open(filename), &gorm.Config{TranslateError: true})
+	conn.AutoMigrate(&model.UserProfile{})
 	conn.AutoMigrate(&model.Permission{})
 	conn.AutoMigrate(&model.Balance{})
 	conn.AutoMigrate(&model.PvPStat{})
+	conn.AutoMigrate(&model.Punishment{})
 	return
 }
--- handler/player_join.go
+++ handler/player_join.go
@@ -1,9 +1,13 @@
 package handler
 
 import (
+	"log"
+
 	"github.com/df-mc/dragonfly/server/player"
 	"github.com/df-mc/dragonfly/server/player/title"
 	"github.com/sandertv/gophertunnel/minecraft/text"
+	"github.com/xtt28/neptune/database"
+	"github.com/xtt28/neptune/database/model"
 	"github.com/xtt28/neptune/economy/econlookup"
 	"github.com/xtt28/neptune/game"
 	"github.com/xtt28/neptune/scoreboard"
@@ -15,6 +19,18 @@ func handleJoin(p *player.Player) {
 	scoreboard.Render(p, stats.GetStats(p), econlookup.GetBitsBalance(p))
 	
 	game.SendToSpawn(p, false)
+
+	profileTarget := &model.UserProfile{}
+	err := database.DB.FirstOrCreate(profileTarget, &model.UserProfile{UUID: p.UUID()}).Error
+	if err != nil {
+		log.Printf("DB could not load/create user profile for %s (%s): %s", p.Name(), p.UUID(), err.Error())
+		p.Disconnect("We could not initialize your user profile. Please try again or contact an administrator.")
+		return
+	}
+	if profileTarget.LastUsername != p.Name() {
+		profileTarget.LastUsername = p.Name()
+		database.DB.Save(profileTarget)
+	}
 	
 	p.SendTitle(title.New(text.Colourf("<diamond><bold>Welcome</bold></diamond>")).WithSubtitle(text.Colourf("<grey>to Neptune</grey>")))
 }
